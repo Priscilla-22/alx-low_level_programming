f925df5141b8078e8566bde5f6dad04e58df4b57
Priscilla-22 <priscillamw123@gmail.com>
2023-02-17 05:42:08 -0800

    Taask 1-12

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  program that prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
