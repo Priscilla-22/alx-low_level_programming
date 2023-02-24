895f0ee470c7eefee050b7646deb24db6eaba896
Priscilla-22 <priscillamw123@gmail.com>
2023-02-24 14:19:08 -0800

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,9 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
